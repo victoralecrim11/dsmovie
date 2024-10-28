3fd32b3f88996ad9fcde9b7149d946836b70e3c6
victoralecrim11 <victorcarmoalecrim@gmail.com>
2024-10-28 13:14:23 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,18 +8,22 @@ export default function Listing() {
 
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+         
         </div>
       </div>
     </>
